9a263ee9c6ac668c282f1451581f38c4bcdb1c42
PlayZoneIO <91011818+IvI03j@users.noreply.github.com>
2026-04-02 20:57:51 +0200

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,12 @@
+const { createClient } = require('@supabase/supabase-js');
+
+const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_SERVICE_ROLE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY;
+
+if (!SUPABASE_URL || !SUPABASE_SERVICE_ROLE_KEY) {
+  console.error('Faltan SUPABASE_URL o SUPABASE_SERVICE_ROLE_KEY');
+}
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY);
+
+module.exports = supabase;
